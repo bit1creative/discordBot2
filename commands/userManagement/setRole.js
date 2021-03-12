be0efec552ff66888036c35120c7616e38a543f6
bit1creative <bit1creative@gmail.com>
2021-03-12 11:01:48 +0200

    working version

--- commands/userManagement/setRole.js
+++ commands/userManagement/setRole.js
@@ -1,43 +1,41 @@
-const { adminManagementChannelId, adminRoleId, guestRoleId } = require("../../config.json");
+const {
+  adminManagementChannelId,
+  adminRoleId,
+  guestRoleId,
+} = require("../../config.json");
 
 function setRole(message) {
+  if (!message.member.roles.cache.find((role) => role.id === adminRoleId)) {
+    return message.delete();
+  }
 
-    if ( !message.member.roles.cache.find( role => role.id === adminRoleId) ) {
+  if (!message.channel.id === adminManagementChannelId) {
+    message.author.send(
+      "Wrong channel. Go to your admin's user management channel."
+    );
+    return message.delete();
+  }
 
-        return message.delete();
+  let user = message.guild.members.cache.find(
+    (user) => user.id === message.mentions?.members?.first()?.user.id
+  );
 
-    }
+  let role = message.mentions?.roles?.first()?.id;
 
-    if( !message.channel.id === adminManagementChannelId ) {
+  if (!user || !role) {
+    message.channel.send("Check for user and role to be mentioned.");
+  }
 
-        message.author.send("Wrong channel. Go to your admin's user management channel.");
-        return message.delete();
+  if (user.roles.cache.find((role) => role.id === guestRoleId)) {
+    user.roles.remove(guestRoleId);
+  }
 
-    }
+  user.roles.add(role);
+  message.channel.send(
+    `User <@${user.id}> has been updated with a role <@&${role}>`
+  );
 
-    let user = message.guild.members.cache.find( 
-        user => user.id === message.mentions?.members?.first()?.user.id
-        );
-
-    let role = message.mentions?.roles?.first()?.id;
-
-    if ( !user || !role ) {
-
-        message.channel.send("Check for user and role to be mentioned.");
-
-    }
-
-    if ( user.roles.cache.find( role => role.id === guestRoleId) ) {
-
-        user.roles.remove(guestRoleId);
-
-    }
-
-    user.roles.add(role);
-    message.channel.send(`User <@${user.id}> has been updated with a role <@&${role}>`);
-
-    return;
-
+  return;
 }
 
-module.exports = { setRole }
+module.exports = { setRole };
